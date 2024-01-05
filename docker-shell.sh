82212f2fc4d97717e2726b828d761be03787b168
Shivas Jayaram <shivasj@gmail.com>
2024-01-05 16:15:53 -0500

    Updated docker-shell file

--- docker-shell.sh
+++ docker-shell.sh
@@ -6,7 +6,7 @@ set -e
 docker network inspect data-labeling-network >/dev/null 2>&1 || docker network create data-labeling-network
 
 # Build the image based on the Dockerfile
-docker build -t data-label-cli --platform=linux/arm64/v8 -f Dockerfile .
+docker build -t data-label-cli -f Dockerfile .
 
 # Run All Containers
 docker-compose run --rm --service-ports data-label-cli
